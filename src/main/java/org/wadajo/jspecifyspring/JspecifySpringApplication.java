1f797a3a41c657a1087e61f797f55225acf4c8ba
Julio César <juliocesareb@hotmail.com>
2025-12-17 20:07:45 +0100

    Primer commit

---
+++ src/main/java/org/wadajo/jspecifyspring/JspecifySpringApplication.java
@@ -0,0 +1,13 @@
+package org.wadajo.jspecifyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspecifySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JspecifySpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/wadajo/jspecifyspring/JspecifySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wadajo.jspecifyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspecifySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
