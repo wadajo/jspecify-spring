60a40a93a924c3c983d7d5f61afb8740dd0a95ae
Julio César <juliocesareb@hotmail.com>
2025-12-23 12:30:16 +0100

    WireMock para test y warning de null

--- src/main/java/org/wadajo/jspecifyspring/GeneralController.java
+++ src/main/java/org/wadajo/jspecifyspring/GeneralController.java
@@ -1,26 +1,41 @@
 package org.wadajo.jspecifyspring;
 
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.time.Year;
-import java.util.List;
+import org.springframework.web.client.RestClient;
+import tools.jackson.databind.json.JsonMapper;
 
 @Controller
 public class GeneralController {
 
-    @GetMapping("/obras")
-    ResponseEntity<ObraResponse> getRandomArtwork() {
+    @Value("${artworks.base-url}")
+    private String endpointUrl;
 
-        return ResponseEntity.ok(
-            new ObraResponse(
-                List.of(
-                new Obra("Nasca","Fragment", Year.of(600),null
-                )
-            )
-        ));
+    private RestClient restClient;
+
+    private JsonMapper jsonMapper;
+
+    @PostConstruct
+    public void init() {
+        restClient = RestClient.builder()
+            .baseUrl(endpointUrl)
+            .build();
+        jsonMapper = new JsonMapper();
+    }
+
+    @GetMapping("/obraRandom")
+    ResponseEntity<Obra> getRandomArtwork() {
+        String rawResponse = restClient.get()
+            .retrieve()
+            .body(String.class);
+
+        var dataRawField = jsonMapper.readTree(rawResponse).get("data").get(0);
+        var obraRandom = jsonMapper.readValue(dataRawField.toString(), Obra.class);
+        return ResponseEntity.ok(obraRandom);
     }
 
 }
